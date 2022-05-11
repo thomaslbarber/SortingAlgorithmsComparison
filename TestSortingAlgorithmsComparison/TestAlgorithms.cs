106f5e10c2c7f268c454bc39f17e96af75140aa5
thomaslbarber <thomaslbarber1@gmail.com>
2022-05-11 13:34:29 +0100

    Added tests for Utilities.cs functions.

--- TestSortingAlgorithmsComparison/TestAlgorithms.cs
+++ TestSortingAlgorithmsComparison/TestAlgorithms.cs
@@ -1,97 +1,95 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Algorithms;
-using System;
 using SortingAlgorithmsComparison;
 
-namespace TestSortingAlgorithmsComparison;
-
-[TestClass]
-public class TestAlgorithms
+namespace TestSortingAlgorithmsComparison
 {
-    /// <summary>
-    /// Tests the BubbleSort.cs class.
-    /// </summary>
-    [TestMethod]
-    public void TestBubbleSort()
+    [TestClass]
+    public class TestAlgorithms
     {
-        Utilities.CurrentArraySize = 0;
-        
-        for (int i = 0; i < Utilities.ArraySizes.Length; i++, Utilities.CurrentArraySize++)
+        /// <summary>
+        /// Tests the BubbleSort.cs class.
+        /// </summary>
+        [TestMethod]
+        public void TestBubbleSort()
         {
-            Utilities.GenerateInitialNumbers(Utilities.ArraySizes[Utilities.CurrentArraySize]);
+            Utilities.CurrentArraySize = 0;
 
-            SortingAlgorithm bubbleSort = new BubbleSort();
-            Assert.IsTrue(Utilities.DetermineWhetherSuccessful(bubbleSort.Sort(Utilities.NumbersToSort)));
+            for (int i = 0; i < Utilities.ArraySizes.Length; i++, Utilities.CurrentArraySize++)
+            {
+                Utilities.GenerateInitialNumbers(Utilities.ArraySizes[Utilities.CurrentArraySize]);
+
+                SortingAlgorithm bubbleSort = new BubbleSort();
+                Assert.IsTrue(Utilities.DetermineWhetherSuccessful(bubbleSort.Sort(Utilities.NumbersToSort)));
+            }
+        }
+
+        /// <summary>
+        /// Tests the CocktailSort.cs class.
+        /// </summary>
+        [TestMethod]
+        public void TestCocktailSort()
+        {
+            Utilities.CurrentArraySize = 0;
+
+            for (int i = 0; i < Utilities.ArraySizes.Length; i++, Utilities.CurrentArraySize++)
+            {
+                Utilities.GenerateInitialNumbers(Utilities.ArraySizes[Utilities.CurrentArraySize]);
+
+                SortingAlgorithm cocktailSort = new CocktailSort();
+                Assert.IsTrue(Utilities.DetermineWhetherSuccessful(cocktailSort.Sort(Utilities.NumbersToSort)));
+            }
+        }
+
+        /// <summary>
+        /// Tests the InsertionSort.cs class.
+        /// </summary>
+        [TestMethod]
+        public void TestInsertionSort()
+        {
+            Utilities.CurrentArraySize = 0;
+
+            for (int i = 0; i < Utilities.ArraySizes.Length; i++, Utilities.CurrentArraySize++)
+            {
+                Utilities.GenerateInitialNumbers(Utilities.ArraySizes[Utilities.CurrentArraySize]);
+
+                SortingAlgorithm insertionSort = new InsertionSort();
+                Assert.IsTrue(Utilities.DetermineWhetherSuccessful(insertionSort.Sort(Utilities.NumbersToSort)));
+            }
+        }
+
+        /// <summary>
+        /// Tests the SelectionSort.cs class.
+        /// </summary>
+        [TestMethod]
+        public void TestSelectionSort()
+        {
+            Utilities.CurrentArraySize = 0;
+
+            for (int i = 0; i < Utilities.ArraySizes.Length; i++, Utilities.CurrentArraySize++)
+            {
+                Utilities.GenerateInitialNumbers(Utilities.ArraySizes[Utilities.CurrentArraySize]);
+
+                SortingAlgorithm selectionSort = new SelectionSort();
+                Assert.IsTrue(Utilities.DetermineWhetherSuccessful(selectionSort.Sort(Utilities.NumbersToSort)));
+            }
+        }
+
+        /// <summary>
+        /// Tests the QuickSort.cs class.
+        /// </summary>
+        [TestMethod]
+        public void TestQuickSort()
+        {
+            Utilities.CurrentArraySize = 0;
+
+            for (int i = 0; i < Utilities.ArraySizes.Length; i++, Utilities.CurrentArraySize++)
+            {
+                Utilities.GenerateInitialNumbers(Utilities.ArraySizes[Utilities.CurrentArraySize]);
+
+                SortingAlgorithm quickSort = new QuickSort();
+                Assert.IsTrue(Utilities.DetermineWhetherSuccessful(quickSort.Sort(Utilities.NumbersToSort)));
+            }
         }
     }
-    
-    /// <summary>
-    /// Tests the CocktailSort.cs class.
-    /// </summary>
-    [TestMethod]
-    public void TestCocktailSort()
-    {
-        Utilities.CurrentArraySize = 0;
-
-        for (int i = 0; i < Utilities.ArraySizes.Length; i++, Utilities.CurrentArraySize++)
-        {
-            Utilities.GenerateInitialNumbers(Utilities.ArraySizes[Utilities.CurrentArraySize]);
-
-            SortingAlgorithm cocktailSort = new CocktailSort();
-            Assert.IsTrue(Utilities.DetermineWhetherSuccessful(cocktailSort.Sort(Utilities.NumbersToSort)));
-        }
-    }
-    
-    /// <summary>
-    /// Tests the InsertionSort.cs class.
-    /// </summary>
-    [TestMethod]
-    public void TestInsertionSort()
-    {
-        Utilities.CurrentArraySize = 0;
-
-        for (int i = 0; i < Utilities.ArraySizes.Length; i++, Utilities.CurrentArraySize++)
-        {
-            Utilities.GenerateInitialNumbers(Utilities.ArraySizes[Utilities.CurrentArraySize]);
-
-            SortingAlgorithm insertionSort = new InsertionSort();
-            Assert.IsTrue(Utilities.DetermineWhetherSuccessful(insertionSort.Sort(Utilities.NumbersToSort)));
-        }
-    }
-    
-    /// <summary>
-    /// Tests the SelectionSort.cs class.
-    /// </summary>
-    [TestMethod]
-    public void TestSelectionSort()
-    {
-        Utilities.CurrentArraySize = 0;
-
-        for (int i = 0; i < Utilities.ArraySizes.Length; i++, Utilities.CurrentArraySize++)
-        {
-            Utilities.GenerateInitialNumbers(Utilities.ArraySizes[Utilities.CurrentArraySize]);
-
-            SortingAlgorithm selectionSort = new SelectionSort();
-            Assert.IsTrue(Utilities.DetermineWhetherSuccessful(selectionSort.Sort(Utilities.NumbersToSort)));
-        }
-    }
-    
-    /// <summary>
-    /// Tests the QuickSort.cs class.
-    /// </summary>
-    [TestMethod]
-    public void TestQuickSort()
-    {
-        Utilities.CurrentArraySize = 0;
-
-        for (int i = 0; i < Utilities.ArraySizes.Length; i++, Utilities.CurrentArraySize++)
-        {
-            Utilities.GenerateInitialNumbers(Utilities.ArraySizes[Utilities.CurrentArraySize]);
-
-            SortingAlgorithm quickSort = new QuickSort();
-            Assert.IsTrue(Utilities.DetermineWhetherSuccessful(quickSort.Sort(Utilities.NumbersToSort)));
-        }
-    }
-
-
 }
--- TestSortingAlgorithmsComparison/TestUtilities.cs
+++ TestSortingAlgorithmsComparison/TestUtilities.cs
@@ -1,6 +1,35 @@
-namespace TestSortingAlgorithmsComparison;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingAlgorithmsComparison;
 
-public class TestUtilities
+namespace TestSortingAlgorithmsComparison
 {
-    
+    [TestClass]
+    public class TestUtilities
+    {
+        /// <summary>
+        /// Tests the DetermineWhetherSuccessful() function using some correctly sorted arrays.
+        /// </summary>
+        [TestMethod]
+        public void TestDetermineWhetherSuccessfulCorrectValues()
+        {
+            int[] correctValues0 = new int[5] { 2, 6, 92, 4331, 282034 };
+            int[] correctValues1 = new int[10] { -54, -27, -2, 0, 1, 6, 22, 59, 117, 235 };
+
+            Assert.IsTrue(Utilities.DetermineWhetherSuccessful(correctValues0));
+            Assert.IsTrue(Utilities.DetermineWhetherSuccessful(correctValues1));
+        }
+
+        /// <summary>
+        /// Tests the DetermineWhetherSuccessful() function using some incorrectly sorted arrays.
+        /// </summary>
+        [TestMethod]
+        public void TestDetermineWhetherSuccessfulIncorrectValues()
+        {
+            int[] correctValues0 = new int[5] { 2, 1, 92, 4331, 282034 };
+            int[] correctValues1 = new int[10] { -54, -27, -2, 0, 7, 6, 22, 59, 117, 235 };
+
+            Assert.IsFalse(Utilities.DetermineWhetherSuccessful(correctValues0));
+            Assert.IsFalse(Utilities.DetermineWhetherSuccessful(correctValues1));
+        }
+    }
 }
